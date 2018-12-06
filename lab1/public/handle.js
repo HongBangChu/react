319f259e9e8a5a40ca5326a141606a9e3d53e58c
Nguyễn Hồng Minh <nhminh@live.com>
2018-12-06 22:36:00 +0700

    Update handle.js

--- lab1/public/handle.js
+++ lab1/public/handle.js
@@ -1,18 +1,27 @@
 //import React from 'react';
+var publicFunc = function(name){
+  alert('public function: '+name);
+}
 var MComp = React.createClass({
+  callPublicFunc: function(){
+    publicFunc(this.props.name);
+  },
   render: function(){
     return(
-      <div>This is {this.props.name} React component <b>{this.props.children}</b> <MComp1/></div>
+      <div><button onClick={this.callPublicFunc}>call public function from MComp</button>This is {this.props.name} React component <b>{this.props.children}</b> <MComp1>Children of MComp1</MComp1></div>
     );
   }
 });
 var MComp1 = React.createClass({
+  getText: function(){
+    alert(this.props.children);
+  },
   render: function(){
     return(
-      <i>Another component</i>
+      <i><button onClick={()=>{publicFunc(this.props.children)}}>arrow function</button>Another component<button onClick={this.getText}>Click me</button><button onClick={publicFunc}>call public function</button></i>
     );
   }
 });
 ReactDOM.render(
-  <div><MComp/><MComp>Children</MComp><MComp name='Lee'>sdfadsfads</MComp></div>
+  <div><MComp/><MComp>Children</MComp><MComp name='Lee'>sdfadsfads</MComp><MComp1>Children of Another MComp1</MComp1></div>
   , document.getElementById('root'));
